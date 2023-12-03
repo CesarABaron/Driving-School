06eae2e3c99df8f3fa0db12f750ceb820452e292
KenMiyagi <kenrodrigoariel@gmail.com>
2023-12-02 23:26:15 -0300

    Procedures CRUD routes

--- server/src/Routes/proceduresRoute.js
+++ server/src/Routes/proceduresRoute.js
@@ -1,7 +1,11 @@
 const { Router } = require("express");
-
+/* const { getAllProceduresHandler, getProcedureByIdHandler, updateProceduresHandler, createProcedureHandler, deleteProcedureHandler} = require("../Handlers/proceduresHandlers") */
 const router = Router();
 
-/* router.create("/",createProcedureHandler) */
+/* router.get("/",getAllProceduresHandler)
+router.get("/:id",getProcedureByIdHandler)
+router.patch("/:id",updateProceduresHandler)
+router.post("/",createProcedureHandler)
+router.delete("/:id",deleteProcedureHandler) */
 
 module.exports = router
