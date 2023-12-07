47d921d068a5ee7dfdb91f3b8e04cab102739280
KenMiyagi <kenrodrigoariel@gmail.com>
2023-12-06 22:31:48 -0300

    Procedures controller bug fixed

--- server/src/Controllers/Procedures/createProceduresController.js
+++ server/src/Controllers/Procedures/createProceduresController.js
@@ -1,9 +1,9 @@
 const { procedures } = require("../../db");
 
 const createProceduresController = async (body) => {
-  const { basado } = body;
+  const { name } = body;
   const [newProcedure, created] = await procedures.findOrCreate({
-    where: { basado },
+    where: { name },
     defaults: { ...body },
   });
   if (created) return newProcedure.dataValues;
