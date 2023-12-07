883e502c9a7c79c80d2a094483d17274348cd7b2
CesarABaron <127266668+CesarABaron@users.noreply.github.com>
2023-12-06 21:37:52 -0600

    form and routes

--- client/src/views/Form.jsx
+++ client/src/views/Form.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Form = () => {
   return (
-    <div>
-      
+    <div className="p-5">
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Nombre</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Nombre completo"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Celular</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Numero celular"
+          />
+        </div>
+        <div className="form-check"></div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
