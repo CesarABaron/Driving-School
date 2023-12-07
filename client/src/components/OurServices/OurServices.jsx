f33a3c4abd2157c4fd5a68e34f7be2b9f5701b6e
KenMiyagi <kenrodrigoariel@gmail.com>
2023-12-06 23:57:47 -0300

    OurServices comments deletion

--- client/src/components/OurServices/OurServices.jsx
+++ client/src/components/OurServices/OurServices.jsx
@@ -8,13 +8,6 @@ import { getCourses } from '../../Redux/Actions/coursesActions';
 import Procedures from "./Procedures";
 
 const OurServices = () => {
- /*  const courses = useSelector((state) => state.courses);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-      alert("ENTRA AL USEEFFECT")
-      dispatch(getCourses());
-  }, [courses]); */
   return (
     <div
       id="our-services"
@@ -46,68 +39,12 @@ const OurServices = () => {
         </div>
       </div>
       <br />
-{/*       <h4 class="d-flex justify-content-center">
+      <h4 class="d-flex justify-content-center">
         ¿Trabajas o estudias? ¡No te preocupes! Tenemos HORARIOS FLEXIBLES 6:00
         am a 10:00 pm
-      </h4> */}
+      </h4>
     </div>
   );
 };
 
 export default OurServices;
-
-/* <div class="row d-flex justify-content-center align-items-center  bg-primary">
-        <div className="card mx-2 my-5 " style={{ width: "18em" }}>
-          <img
-            src="https://cdn.discordapp.com/attachments/1180957030673035304/1180964039564742696/ivanfernandezballestrasse_a_man_in_garen_from_league_of_legends_f4a5823f-0d0e-48b2-afa9-0ecc3e1006a7_ins.jpg?ex=657f5578&is=656ce078&hm=093eac4cf1a755897c44dac0c3a50bc44955db711c0ab4d4bbb32a75b83acdc3&"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title ">Curso A2</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Ver más
-            </a>
-          </div>
-        </div>
-
-        <div className="card mx-3 my-5 " style={{ width: "18em" }}>
-          <img
-            src="https://cdn.discordapp.com/attachments/1180957030673035304/1180964039564742696/ivanfernandezballestrasse_a_man_in_garen_from_league_of_legends_f4a5823f-0d0e-48b2-afa9-0ecc3e1006a7_ins.jpg?ex=657f5578&is=656ce078&hm=093eac4cf1a755897c44dac0c3a50bc44955db711c0ab4d4bbb32a75b83acdc3&"
-            className="card-img-top img-fluid w-100"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Curso B1</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Ver más
-            </a>
-          </div>
-        </div>
-
-        <div className="card mx-3   " style={{ width: "18em" }}>
-          <img
-            src="https://cdn.discordapp.com/attachments/1180957030673035304/1180964039564742696/ivanfernandezballestrasse_a_man_in_garen_from_league_of_legends_f4a5823f-0d0e-48b2-afa9-0ecc3e1006a7_ins.jpg?ex=657f5578&is=656ce078&hm=093eac4cf1a755897c44dac0c3a50bc44955db711c0ab4d4bbb32a75b83acdc3&"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Tramites y licencias</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Ver más
-            </a>
-          </div>
-        </div>
-      </div> */
