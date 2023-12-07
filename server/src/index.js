9486ff1d1d8de2c8a4fbd85883c04c217e19e8dc
CesarABaron <127266668+CesarABaron@users.noreply.github.com>
2023-12-06 21:13:28 -0600

    responsive map ready

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ const server = require("./app");
 const { conn } = require("./db");
 const PORT = process.env.PORT || 3001;
 
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log(`%s listening on PORT ${PORT}`);
   });
