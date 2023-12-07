9d2dc684b9371a787f03b7f4ea400d9df0f85022
KenMiyagi <kenrodrigoariel@gmail.com>
2023-12-07 00:18:03 -0300

    getAllQuestionsHandler error message fix

--- server/src/Handlers/questionsHandles.js
+++ server/src/Handlers/questionsHandles.js
@@ -18,7 +18,7 @@ const getAllQuestionsHandler = async (req, res) =>{
     try {
         const courses = await getAllQuestionsController()
         if(courses) return res.status(200).json(courses)
-        return res.status(400).json({error: "There is no courses."})
+        return res.status(400).json({error: "There is no FAQs."})
         
     } catch (error) {
         res.status(500).json({error: error.message})
